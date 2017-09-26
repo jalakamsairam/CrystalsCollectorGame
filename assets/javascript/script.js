6613dd8beeda0d00ea898c07d132df350c960ec5
jalakamsairam <sairam.jalakamdevarajulu@mavs.uta.edu>
2017-09-25 21:40:38 -0700

    everything except the reset button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,6 +6,17 @@ $(document).ready(function(){
 
 
 var randomNumber;
+var score;
+var wins = 0;
+var losses = 0;
+var userScore = 0 ;
+
+var num1 = 0;
+var num2 = 0;
+var num3 = 0;
+var num4 = 0;
+var flag = false;
+
 
 
 function randomGeneratorForComputer() {
@@ -17,20 +28,81 @@ function randomGeneratorForComputer() {
   return randomNumber;
 }
 
+function randomNumberGeneratorForButtons() {
+  let arr=[];
+  num1 =(Math.ceil(Math.random()*12));
+  console.log(num1);
+  num2 =(Math.ceil(Math.random()*12));
+  num3 =(Math.ceil(Math.random()*12));
+  num4 =(Math.ceil(Math.random()*12)); 
+ //  console.log(jQuery.type(num1));
+ // console.log(num1,num2,num3,num4);
+ arr.push(num1);
+ arr.push(num2);
+ arr.push(num3);
+ arr.push(num4);
+ return arr;
+}
+
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
+function youWin(){
 
-function renderScreen(){
-  $("#randomnumber").html(randomNumber);
-  console.log("inside renderScreen");
-  console.log(randomNumber)
+  wins++;
+
+  $("#message").text("you win!");
+  $("#wins").text(wins);
 }
 
-randomGeneratorForComputer();//onstart
-renderScreen();//onstart
+function youLoose(){
+  losses++;
 
+  $("#message").text("you win!");
+  $("#losses").text(losses);
+}
+
+function renderScreenAndValues(){
+  let x = randomNumberGeneratorForButtons();
+  console.log("TEST: "+x);
+    $("#randomnumber").html(randomNumber);
+  //console.log("inside renderScreen");
+    $("#userScore").text(userScore);
+
+    $("#wins").text(wins);
+
+    $("#losses").text(losses);
+
+    $("#button1").attr('value',x[0]);
+
+    $("#button2").attr("value",x[1]);
+
+    $("#button3").attr("value",x[2]);
+
+    $("#button4").attr("value",x[3]);
+  //console.log(randomNumber)
+}
+
+$("button").on("click",function(){
+        flag = true;
+      userScore += parseInt($(this).val());
+      $("#userScore").text(userScore);
+      // if(userScore == randomNumber)
+     
+   if(userScore === randomNumber){
+    youWin();
+    flag = false;
+    }
+   else if(userScore > randomNumber){
+    youLoose();
+    flag = false;
+  }
+});
+
+randomGeneratorForComputer();//onstart
+renderScreenAndValues();//onstart
 });
